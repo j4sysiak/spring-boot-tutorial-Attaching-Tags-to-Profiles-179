bcf79628cf37fc0a46cc3fd1846b254984501d2e
j4sysiak <j4sysiak@gmail.com>
2019-03-11 07:01:05 +0100

    1

---
+++ spring-boot-tutorial-Attaching-Tags-to-Profiles-179/src/test/java/com/jaceksysiak/tests/ProfileControllerRestTest.java
@@ -0,0 +1,90 @@
+package com.jaceksysiak.tests;
+
+import javax.transaction.Transactional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.test.context.support.WithMockUser;
+//import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.jaceksysiak.App;
+import com.jaceksysiak.service.InterestService;
+import com.jaceksysiak.service.ProfileService;
+import com.jaceksysiak.service.UserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(App.class)
+@WebAppConfiguration
+@Transactional
+public class ProfileControllerRestTest {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private ProfileService profileService;
+
+	@Autowired
+	private InterestService interestService;
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	@WithMockUser(username = "p1@wp.pl")
+	public void testSaveAndDeleteInterest() throws Exception {
+		
+	}
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
